8da27e222046c3e8e8f123eb04c47742cb838e87
aaaa <rajdeepnewgeneration@gmail.com>
2023-11-09 22:40:56 +0530

    Update app.py gptq

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ from huggingface_hub import snapshot_download
 class InferlessPythonModel:
     def initialize(self):
         snapshot_download(
-            "TheBloke/CodeLlama-34B-Python-AWQ",
+            "TheBloke/CodeLlama-34B-Python-GPTQ",
             local_dir="/model",
             token="<<your_token>>",
         )
         self.llm = LLM(
             model="/model",
-            quantization="awq",
+            quantization="gptq",
             # tensor_parallel_size=args.tensor_parallel_size,
             # max_num_seqs=args.batch_size,
             # max_num_batched_tokens=args.batch_size * args.input_len,
