06ff70572bea773e0df1c7aa4f2ee5901f0b8c42
aaaa <rajdeepnewgeneration@gmail.com>
2023-11-09 12:36:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,43 @@ from vllm import SamplingParams
 from vllm import LLM
 from huggingface_hub import snapshot_download
 
+    # sampling_params = SamplingParams(
+    #     n=1,
+    #     temperature=0.5,
+    #     top_p=1,
+    #     use_beam_search=args.use_beam_search,
+    #     ignore_eos=True,
+    #     max_tokens=256,
+    # )
+
+
 
 class InferlessPythonModel:
     def initialize(self):
-        self.template = """SYSTEM: You are a helpful assistant.
-        USER: {}
-        ASSISTANT: """
         snapshot_download(
-            "meta-llama/Llama-2-7b-chat-hf",
+            "TheBloke/CodeLlama-34B-Python-GPTQ",
             local_dir="/model",
             token="<<your_token>>",
         )
-        self.llm = LLM("/model")
+        self.llm = LLM(
+            model="/model",
+            quantization="gptq",
+            # tensor_parallel_size=args.tensor_parallel_size,
+            # max_num_seqs=args.batch_size,
+            # max_num_batched_tokens=args.batch_size * args.input_len,
+            # max_model_len=256,
+            # trust_remote_code=args.trust_remote_code,
+            dtype="half",
+            )
     
     def infer(self, inputs):
-        print("inputs[questions] -->", inputs["questions"], flush=True)
-        prompts = [self.template.format(inputs["questions"])]
+        print("inputs[prompt] -->", inputs["prompt"], flush=True)
+        prompts = inputs[prompt]
         print("Prompts -->", prompts, flush=True)
         sampling_params = SamplingParams(
-            temperature=0.75,
+            temperature=0.5,
             top_p=1,
-            max_tokens=800,
+            max_tokens=256,
             presence_penalty=1.15,
         )
         result = self.llm.generate(prompts, sampling_params)
