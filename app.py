ea041969354e83a6834a1c3c6e589bbdeea86a94
aaaa <rajdeepnewgeneration@gmail.com>
2023-11-18 19:30:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ from huggingface_hub import snapshot_download
 class InferlessPythonModel:
     def initialize(self):
         snapshot_download(
-            "TheBloke/CodeLlama-34B-Python-GPTQ",
+            "TheBloke/CodeLlama-34B-Python-AWQ",
             local_dir="/model",
             token="<<your_token>>",
         )
         self.llm = LLM(
             model="/model",
-            quantization="gptq",
+            quantization="awq",
             # tensor_parallel_size=args.tensor_parallel_size,
             # max_num_seqs=args.batch_size,
             # max_num_batched_tokens=args.batch_size * args.input_len,
@@ -37,10 +37,9 @@ class InferlessPythonModel:
         prompts = inputs["prompt"]
         print("Prompts -->", prompts, flush=True)
         sampling_params = SamplingParams(
-            temperature=0.5,
+            temperature=1,
             top_p=1,
-            max_tokens=256,
-            presence_penalty=1.15,
+            max_tokens=512
         )
         result = self.llm.generate(prompts, sampling_params)
         result_output = [output.outputs[0].text for output in result]
